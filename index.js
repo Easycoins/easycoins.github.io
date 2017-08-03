c942a4431328f875da37d84062b419e0eaf3955f
Bojan Stefanovic <bojanstef4@gmail.com>
2017-08-02 17:23:27 -0700

    Simple website to show Tradeview CEXIO:ETH/USD

---
+++ index.js
@@ -0,0 +1,17 @@
+// TradingView Widget BEGIN
+new TradingView.widget({
+    "autosize": true,
+    "symbol": "CEXIO:ETHUSD",
+    "interval": "D",
+    "timezone": "America/Vancouver",
+    "theme": "Black",
+    "style": "1",
+    "locale": "en",
+    "toolbar_bg": "#f1f3f6",
+    "enable_publishing": false,
+    "hide_top_toolbar": true,
+    "withdateranges": true,
+    "allow_symbol_change": true,
+    "hideideas": true
+});
+// TradingView Widget END
